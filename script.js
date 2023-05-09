64f6b85cc7a203662520236a3e3075287fd8013c
SASIsann <131449719+SASIsann@users.noreply.github.com>
2023-05-09 23:46:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// script.js
 let tasks = [];
 let archivedTasks = [];
 
 document.getElementById('add-task').addEventListener('click', function() {
     const taskInput = document.getElementById('task-input');
+    const taskStartTime = document.getElementById('task-start-time');
+    const taskEndTime = document.getElementById('task-end-time');
     if (taskInput.value) {
-        tasks.push(taskInput.value);
+        tasks.push({
+            title: taskInput.value,
+            startTime: taskStartTime.value,
+            endTime: taskEndTime.value,
+        });
         taskInput.value = '';
+        taskStartTime.value = '';
+        taskEndTime.value = '';
         renderTasks();
     }
 });
@@ -16,7 +23,7 @@ function renderTasks() {
     tasksContainer.innerHTML = '';
     tasks.forEach(function(task, index) {
         const taskElement = document.createElement('div');
-        taskElement.innerText = task;
+        taskElement.innerText = `${task.title} (${task.startTime} - ${task.endTime})`;
         taskElement.addEventListener('click', function() {
             archivedTasks.push(task);
             tasks.splice(index, 1);
@@ -32,8 +39,18 @@ function renderArchivedTasks() {
     archivedTasksContainer.innerHTML = '';
     archivedTasks.forEach(function(task) {
         const taskElement = document.createElement('div');
-        taskElement.innerText = task;
+        taskElement.innerText = `${task.title} (${task.startTime} - ${task.endTime})`;
         archivedTasksContainer.appendChild(taskElement);
     });
 }
+
+function displayCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    document.getElementById('current-time').innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(displayCurrentTime, 1000);
 
